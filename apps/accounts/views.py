c99a9d5fc8febec27b5c4651fedf0d3a8411cfe2
Praise Idowu <ifeoluwapraise02@gmail.com>
2023-11-22 13:02:07 +0100

    pages: register & login

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from distutils.log import Log
+from django.shortcuts import redirect, render
+from django.views import View
+from . forms import RegisterForm, LoginForm
 
-# Create your views here.
+class RegisterView(View):
+    def get(self, request):
+        form = RegisterForm()
+        context = {"form": form}
+        return render(request, 'accounts/register.html', context)
+    
+    def post(self, request):
+        return redirect('')
+    
+class LoginView(View):
+    def get(self, request):
+        form = LoginForm()
+        context = {"form": form}
+        return render(request, 'accounts/login.html', context)
+    
+    def post(self, request):
+        return redirect('')
+    
